06f07eb3bf52ad67d8dcbb00072f91b0232b4064
AdminWhaleFall <2734184475@qq.com>
2021-10-02 10:01:12 +0800

    :bug: 重新导出正确依赖.

--- app.py
+++ app.py
@@ -7,15 +7,19 @@ LastEditTime: 2021-09-25 01:42:06
 Description: 项目启动文件
 """
 from app import create_app, db
+from app.models import User
 from flask_migrate import Migrate
 
 app = create_app('DevelopmentConfig')
 migrate = Migrate(app, db)
 
-# @app.shell_context_processors
-# def make_shell_context():
-#     """shell上下文"""
-#     return dict(db=db, User=User)
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='9000')
+    with app.app_context():
+        db.create_all()
+        if not User.query.filter_by(username="admin").first():
+            """如果没有管理员账号就新建一个"""
+            user = User(username='admin', email='admin@admin.com', password="admin")
+            db.session.add(user)
+            db.session.commit()
+
+    app.run(host='0.0.0.0', port='9000', debug=True)
